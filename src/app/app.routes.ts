dbb7870d067306129b9be873f92627b0b24010b8
vivian.cruz@aluno.senai.br <vivian.cruz@aluno.senai.br>
2026-08-12 21:43:07 -0300

    em andamento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Menucomponent } from './component/menucomponent/menucomponent';
+
+export const routes: Routes = [
+    {
+        path:'',
+        re
+        
+    }
+];
--- src/app/component/menucomponent/menucomponent.ts
+++ src/app/component/menucomponent/menucomponent.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menucomponent',
-  imports: [],
+  imports: [ RouterLink ],
   templateUrl: './menucomponent.html',
   styleUrl: './menucomponent.css',
 })
