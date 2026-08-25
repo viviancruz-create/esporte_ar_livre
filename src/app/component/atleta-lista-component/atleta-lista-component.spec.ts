2e127e1fedca2da34f13d4b9b41063c309bb5fbd
vivian suenny <vivian.cruz@aluno.senai.br>
2026-08-24 21:36:50 -0300

    em andamento

--- src/app/component/atleta-lista-component/atleta-lista-component.spec.ts
+++ src/app/component/atleta-lista-component/atleta-lista-component.spec.ts
@@ -5,8 +5,7 @@ import { AtletaService } from '../../service/atleta-service';
 import { provideHttpClient } from '@angular/common/http';
 describe('AtletaListaComponent', () => {
  
-  let service = AtletaService
-
+  let service : AtletaService
   beforeEach(async () => {
     await TestBed.configureTestingModule({
     
@@ -16,12 +15,12 @@ describe('AtletaListaComponent', () => {
       ]
     }).compileComponents();
 
-   service = TestBed.inject(AtletaService)
+   service = TestBed.inject(AtletaService);
   });
 
   it('Resultado esperado é calcular corretamente a idade', () => {
     const resultado = service.calcularIdade('2007-05-02')
     expect(resultado).toBe(19);
-  }
+  
   });
 });
